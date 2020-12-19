1ae5626c648cf6a3c4412eae0893aec81c611ea5
Jamie Masalinto <jmasalinto@gmail.com>
2020-12-19 15:42:35 -0500

    commit tree 5bac6ec68e687b80aa4b9c6f68dfca6e5f4372f1

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,20 @@
+require 'pry'
+
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+
+# doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+# doc.css(".headline-26OIBN").text
+
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+doc.css(".tout__label.heading.heading--level-4")
+
+courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do |course|
+    puts course.text.strip
+end 
